84b5b6795e5faaf4f16c655975003a2d87382288
supermancc <hechuan@buaa.edu.cn>
2016-11-19 00:00:24 +0800

    test

---
+++ login.php
@@ -0,0 +1,35 @@
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+
+<?php
+  $name =$_POST['name'];
+  $password =$_POST['password'];
+  $type =$_POST['find'];
+  echo $name.'<br />';
+  echo $password.'<br />';
+  echo $type.'<br />';
+  //phpinfo();
+  
+  $serverName = "(local)";
+  $connectionInfo =  array("UID"=>"sa","PWD"=>"123456","Database"=>"mytest");
+  $conn = sqlsrv_connect( $serverName, $connectionInfo);
+  if( $conn ){
+      echo "Connection established.".'<br/>';
+  }else{
+      echo "Connection could not be established.\n";
+      die( var_dump(sqlsrv_errors()));
+  }
+  $query=sqlsrv_query($conn,"select * from mytest.dbo.J");
+  while($row=sqlsrv_fetch_array($query))
+  {
+      echo $row['JNO'].'<br />';
+  }
+  sqlsrv_close( $conn);
+  
+?>
+</body>
+</html>
+
