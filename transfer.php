3060af49be559f99f59efbe2bbb3fa9035fca79a
supermancc <hechuan@buaa.edu.cn>
2016-11-21 13:14:56 +0800

    bug solved

--- transfer.php
+++ transfer.php
@@ -19,26 +19,27 @@
     if($transfer_row==null)
     {
     	echo "your transfer account does not exit!";
-    	Header("Location:http://localhost/database/login.php");
     }
     else
     {
     	$querystring="select * from mytest.dbo.Account where AID=$name";
     	$query=sqlsrv_query($conn,$querystring);
-    	$row==sqlsrv_fetch_array($query);
+    	$row=sqlsrv_fetch_array($query);
     	if($row['Balance']<$transfer_ammount)
     	{
-    		echo "you do not have enough balance!"
-    		Header("Location:http://localhost/database/login.php");
+    		echo "you do not have enough balance!";
     	}
     	else
     	{
-
-    		$querystring="update mytest.dbo.Account set Balance=$transfer_row['Balance']+$transfer_ammount where AID=$transfer_name";
+    		$up=$transfer_row['Balance']+$transfer_ammount;
+    		$querystring="update mytest.dbo.Account set Balance=$up where AID=$transfer_name";
+    		//var_dump($querystring);
     		$query=sqlsrv_query($conn,$querystring);
-    		$querystring="update mytest.dbo.Account set Balance=$row['Balance']-$transfer_ammount where AID=$name";
+    		$up=$row['Balance']-$transfer_ammount;
+    		$querystring="update mytest.dbo.Account set Balance=$up where AID=$name";
+    		//var_dump($querystring);
     		$query=sqlsrv_query($conn,$querystring);
-    		Header("Location:http://localhost/database/login.php");
     	}
     }
+    Header("Location:http://localhost/database/login.php");
 	?>
